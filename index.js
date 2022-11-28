194910a61e067078357a19cd9e617e33e5b11baf
Elisabeth Aka <aea536@nyu.edu>
2022-11-28 15:02:44 -0500

    commit tree aa62af0ca030d80dc0fd3d616b2687926a5f8169

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+//function expression
+function saturdayFun(activity="roller-skate") {
+    //console.log(activity);
+    return `This Saturday, I want to ${activity}!`;
+}
+
+//function declarations
+//const day = function mondayWork(go to the office. ) {
+//   return `This Monday, I will ${mondayWork}!`; 
+//}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+
+}
+ function wrapAdjective(string="*") {
+    return function wish(adj="special"){
+        return `You are ${string}${adj}${string}!`;
+    }
+ }
